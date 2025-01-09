a5f1ec146dad1ef6effc4c4590aa177d116400a8
hung <Padsboy007@gmail.com>
2025-01-09 19:06:24 +0700

    update

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,20 @@
 Rails.application.routes.draw do
   resource :session
   resources :passwords, param: :token
+  
   resources :products do
-    resources :subscribers, only: [ :create ]
+    resources :subscribers, only: [:create]
   end
 
+  # The following routes are redundant because `resources :products` already includes them
+  # get "/products", to: "products#index"
+  # get "/products/new", to: "products#new"
+  # post "/products", to: "products#create"
+  # get "/products/:id", to: "products#show"
+  # get "/products/:id/edit", to: "products#edit"
+  # patch "/products/:id", to: "products#update"
+  # put "/products/:id", to: "products#update"
+  # delete "/products/:id", to: "products#destroy"
 
-  get "/products", to: "products#index"
-
-get "/products/new", to: "products#new"
-post "/products", to: "products#create"
-
-get "/products/:id", to: "products#show"
-
-get "/products/:id/edit", to: "products#edit"
-patch "/products/:id", to: "products#update"
-put "/products/:id", to: "products#update"
-
-delete "/products/:id", to: "products#destroy"
-root "products#index"
+  root "products#index"
 end
